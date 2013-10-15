8d0bbbd2d96b712d5aaa994594d98d99c07415c2
sicongwu <sicongwu@anjuke.com>
2013-10-15 10:28:50 +0800

    Initial Commit

---
+++ AnjukeBroker_New/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AnjukeBroker_New
+//
+//  Created by Wu sicong on 13-10-15.
+//  Copyright (c) 2013年 Wu sicong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ AnjukeBroker_NewTests/AnjukeBroker_NewTests.m
@@ -0,0 +1,34 @@
+//
+//  AnjukeBroker_NewTests.m
+//  AnjukeBroker_NewTests
+//
+//  Created by Wu sicong on 13-10-15.
+//  Copyright (c) 2013年 Wu sicong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AnjukeBroker_NewTests : XCTestCase
+
+@end
+
+@implementation AnjukeBroker_NewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
