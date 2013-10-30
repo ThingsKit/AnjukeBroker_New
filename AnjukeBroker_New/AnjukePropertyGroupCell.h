aa5802df488bd7b8119fb52a7fd11ec953dfaf53
sicongwu <sicongwu@anjuke.com>
2013-10-30 14:44:07 +0800

    add 推广组VC

---
+++ AnjukeBroker_New/AnjukePropertyGroupCell.h
@@ -0,0 +1,19 @@
+//
+//  AnjukePropertyGroupCell.h
+//  AnjukeBroker_New
+//
+//  Created by Wu sicong on 13-10-30.
+//  Copyright (c) 2013年 Wu sicong. All rights reserved.
+//
+
+#import "RTListCell.h"
+
+#define PROPERTY_GROUP_CELL 75
+
+@interface AnjukePropertyGroupCell : RTListCell
+
+@property (nonatomic, strong) UILabel *groupNameLb;
+@property (nonatomic, strong) UILabel *limitPriceLb;
+@property (nonatomic, strong) UILabel *statusLb;
+
+@end
