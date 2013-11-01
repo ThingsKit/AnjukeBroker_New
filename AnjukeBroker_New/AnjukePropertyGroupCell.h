5e8a890ca6f36babb4e0dc6c90bb648a040bee31
sicongwu <sicongwu@anjuke.com>
2013-11-01 10:26:34 +0800

    img status change

--- AnjukeBroker_New/AnjukePropertyGroupCell.h
+++ AnjukeBroker_New/AnjukePropertyGroupCell.h
@@ -10,10 +10,14 @@
 
 #define PROPERTY_GROUP_CELL 65
 
+#define STATUS_STOP_ICON [UIImage imageNamed:@"anjuke_icon09_stop@2x.png"]
+#define STATUS_OK_ICON [UIImage imageNamed:@"anjuke_icon09_woking@2x.png"]
+#define STATUS_ATTENTION_ICON [UIImage imageNamed:@"anjuke_icon08_attention@2x.png"]
+
 @interface AnjukePropertyGroupCell : RTListCell
 
 @property (nonatomic, strong) UILabel *groupNameLb;
 @property (nonatomic, strong) UILabel *limitPriceLb;
-@property (nonatomic, strong) UILabel *statusLb;
+@property (nonatomic, strong) UIImageView *statusIcon;
 
 @end
