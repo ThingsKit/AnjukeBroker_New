f3b93c7c1dd8af55f09da988effdbb2249fd98f9
sicongwu <sicongwu@anjuke.com>
2013-11-12 14:55:38 +0800

    new manager for img

---
+++ AnjukeBroker_New/ImageUploadManager.m
@@ -0,0 +1,13 @@
+//
+//  ImageUploadManager.m
+//  AnjukeBroker_New
+//
+//  Created by Wu sicong on 13-11-12.
+//  Copyright (c) 2013年 Wu sicong. All rights reserved.
+//
+
+#import "ImageUploadManager.h"
+
+@implementation ImageUploadManager
+
+@end
