7b77e801acc632e9358ccda780028fca2fadc41f
sicongwu <sicongwu@anjuke.com>
2013-11-14 14:27:30 +0800

    add core data property

---
+++ AnjukeBroker_New/Property.h
@@ -0,0 +1,27 @@
+//
+//  Property.h
+//  AnjukeBroker_New
+//
+//  Created by Wu sicong on 13-11-14.
+//  Copyright (c) 2013年 Wu sicong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Property : NSManagedObject
+
+@property (nonatomic, retain) NSString * fileNo;
+@property (nonatomic, retain) NSString * comm_id;
+@property (nonatomic, retain) NSString * rooms;
+@property (nonatomic, retain) NSString * area;
+@property (nonatomic, retain) NSString * price;
+@property (nonatomic, retain) NSString * fitment;
+@property (nonatomic, retain) NSString * exposure;
+@property (nonatomic, retain) NSString * floor;
+@property (nonatomic, retain) NSString * title;
+@property (nonatomic, retain) NSString * desc;
+@property (nonatomic, retain) NSString * imageJson;
+
+@end
---
+++ AnjukeBroker_New/Property.m
@@ -0,0 +1,26 @@
+//
+//  Property.m
+//  AnjukeBroker_New
+//
+//  Created by Wu sicong on 13-11-14.
+//  Copyright (c) 2013年 Wu sicong. All rights reserved.
+//
+
+#import "Property.h"
+
+
+@implementation Property
+
+@dynamic fileNo;
+@dynamic comm_id;
+@dynamic rooms;
+@dynamic area;
+@dynamic price;
+@dynamic fitment;
+@dynamic exposure;
+@dynamic floor;
+@dynamic title;
+@dynamic desc;
+@dynamic imageJson;
+
+@end
