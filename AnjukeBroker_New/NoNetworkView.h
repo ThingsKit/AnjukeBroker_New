65068c746f1cefbd27608a877df00c3682ee0f1f
sicongwu <sicongwu@anjuke.com>
2013-11-30 16:03:28 +0800

    add new file

---
+++ AnjukeBroker_New/NoNetworkView.h
@@ -0,0 +1,13 @@
+//
+//  NoNetworkView.h
+//  AnjukeBroker_New
+//
+//  Created by Wu sicong on 13-11-30.
+//  Copyright (c) 2013年 Wu sicong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NoNetworkView : UIView
+
+@end
---
+++ AnjukeBroker_New/NoNetworkView.m
@@ -0,0 +1,33 @@
+//
+//  NoNetworkView.m
+//  AnjukeBroker_New
+//
+//  Created by Wu sicong on 13-11-30.
+//  Copyright (c) 2013年 Wu sicong. All rights reserved.
+//
+
+#import "NoNetworkView.h"
+
+@implementation NoNetworkView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+        
+        UIImageView *img = [[UIImageView alloc] initWithImage:<#(UIImage *)#>]
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
