5f703086ff651ecf4a0d7bc132ff98cda687bce3
paper <paper@paperdeMacBook-Pro.local>
2013-12-17 13:04:55 +0800

    add new controller for release property

---
+++ AnjukeBroker_New/AJKReleasePropertyViewController.h
@@ -0,0 +1,13 @@
+//
+//  AJKReleasePropertyViewController.h
+//  AnjukeBroker_New
+//
+//  Created by paper on 13-12-17.
+//  Copyright (c) 2013年 Wu sicong. All rights reserved.
+//
+
+#import "RTViewController.h"
+
+@interface AJKReleasePropertyViewController : RTViewController
+
+@end
---
+++ AnjukeBroker_New/AJKReleasePropertyViewController.m
@@ -0,0 +1,38 @@
+//
+//  AJKReleasePropertyViewController.m
+//  AnjukeBroker_New
+//
+//  Created by paper on 13-12-17.
+//  Copyright (c) 2013年 Wu sicong. All rights reserved.
+//
+
+#import "AJKReleasePropertyViewController.h"
+
+@interface AJKReleasePropertyViewController ()
+
+@end
+
+@implementation AJKReleasePropertyViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
