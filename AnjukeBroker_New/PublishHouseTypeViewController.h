4483784c28ba29dd0a56e43e8a2e8f41f5cedd1e
吴思聪 <sicongwu@anjuke.com>
2014-01-23 09:54:39 +0800

    add new file

---
+++ AnjukeBroker_New/PublishHouseTypeViewController.h
@@ -0,0 +1,13 @@
+//
+//  PublishHouseTypeViewController.h
+//  AnjukeBroker_New
+//
+//  Created by paper on 14-1-23.
+//  Copyright (c) 2014年 Wu sicong. All rights reserved.
+//
+
+#import "RTViewController.h"
+
+@interface PublishHouseTypeViewController : RTViewController
+
+@end
