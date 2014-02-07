72db3b1ac7c2dc0c598f8ebc2e1aa81ed32fb09c
吴思聪 <sicongwu@anjuke.com>
2014-02-07 10:30:38 +0800

    add new controller

---
+++ AnjukeBroker_New/PublishHouseTypeEditViewController.h
@@ -0,0 +1,13 @@
+//
+//  PublishHouseTypeEditViewController.h
+//  AnjukeBroker_New
+//
+//  Created by paper on 14-2-7.
+//  Copyright (c) 2014年 Wu sicong. All rights reserved.
+//
+
+#import "PublishHouseTypeViewController.h"
+
+@interface PublishHouseTypeEditViewController : PublishHouseTypeViewController
+
+@end
