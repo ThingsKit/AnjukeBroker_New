16e901235d1f669d1d61a785a6ec121a00a9ed82
吴思聪 <sicongwu@anjuke.com>
2014-03-04 19:11:56 +0800

    bug fix

--- AnjukeBroker_New/HomeViewController.m
+++ AnjukeBroker_New/HomeViewController.m
@@ -78,7 +78,9 @@
     
     //设置按钮
     UIBarButtonItem *rightItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"anjuke_icon_setting.png"] style:UIBarButtonItemStylePlain target:self action:@selector(rightButtonAction:)];
-    rightItem.tintColor = [UIColor whiteColor];
+    if (![AppManager isIOS6]) {
+        rightItem.tintColor = [UIColor whiteColor];
+    }
     self.navigationItem.rightBarButtonItem = rightItem;
     
     //监听被踢出下线通知
