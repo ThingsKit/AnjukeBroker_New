148fb1f86e2cd7e79fb1d1651beb86f29fe193a3
your namejianzhongliu <your_email@youremail.com767381290@qq.com>
2014-03-07 11:12:38 +0800

    modify property url syle

--- AnjukeBroker_New/BrokerChatViewController.m
+++ AnjukeBroker_New/BrokerChatViewController.m
@@ -95,9 +95,9 @@
     
     // 租房单页详情url确认结果：http://lvandu.dev.anjuke.com/rent/x/11/23893357-3
     //格式：http://fp07.m.dev.anjuke.com/rent/x/{城市id}/{房源id}-{租房类型}
-    NSString *des = [NSString stringWithFormat:@"%@室%@厅%@卫 %@平",dic[@"roomNum"], dic[@"hallNum"], dic[@"toiletNum"], dic[@"area"]];
+    NSString *des = [NSString stringWithFormat:@"%@室%@厅%@卫 %d平",dic[@"roomNum"], dic[@"hallNum"], dic[@"toiletNum"], [dic[@"area"] integerValue]];
     if (houseType ) {
-        NSString *price = [NSString stringWithFormat:@"%@%@", dic[@"price"], dic[@"priceUnit"]];
+        NSString *price = [NSString stringWithFormat:@"%d%@", [dic[@"price"] integerValue], dic[@"priceUnit"]];
         NSString *url = nil;
         url = [NSString stringWithFormat:@"http://m.anjuke.com/sale/x/%@/%@",[LoginManager getCity_id],dic[@"id"]];
 #if DEBUG
@@ -106,8 +106,8 @@
         propDict = [NSMutableDictionary dictionaryWithDictionary:@{@"id":dic[@"id"], @"des":des, @"img":dic[@"imgUrl"], @"name":dic[@"commName"], @"price":price, @"url":url, @"tradeType":[NSNumber numberWithInteger:AXMessagePropertySourceErShouFang]}];
     }else{
         NSString *price = [NSString stringWithFormat:@"%@%@/月", dic[@"price"], dic[@"priceUnit"]];
-        NSString *url = nil;http://lvandu.dev.anjuke.com/rent/x/11/23893357-3
-        url = [NSString stringWithFormat:@"http://lvandu.dev.anjuke.com/rent/x/%@/%@-3",[LoginManager getCity_id],dic[@"id"]];
+        NSString *url = nil;//http://lvandu.dev.anjuke.com/rent/x/11/23893357-3
+        url = [NSString stringWithFormat:@"http://m.anjuke.com/rent/x/%@/%@-3",[LoginManager getCity_id],dic[@"id"]];
 #if DEBUG
 //        url = [NSString stringWithFormat:@"http://lvandu.dev.anjuke.com/rent/x/%@/%@-3",[LoginManager getCity_id],dic[@"id"]];
 #endif
