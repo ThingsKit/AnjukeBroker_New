3a8d2c5c1ee1ff33bdf18e873aa45ed4c2bfcd7e
吴思聪 <sicongwu@anjuke.com>
2014-04-18 17:31:32 +0800

    UIComponents && iOS5适配

---
+++ AnjukeBroker_New/UIComponents.h
@@ -0,0 +1,27 @@
+//
+//  UIComponents.h
+//  UIComponents
+//
+//  Created by zheng yan on 12-6-25.
+//  Copyright (c) 2012年 anjuke. All rights reserved.
+//
+
+#if __IPHONE_OS_VERSION_MAX_ALLOWED < __IPHONE_6_0
+#define NSLineBreakMode                 UILineBreakMode
+#define NSLineBreakByWordWrapping       UILineBreakModeWordWrap
+#define NSLineBreakByCharWrapping       UILineBreakModeCharacterWrap
+#define NSLineBreakByClipping       UILineBreakModeClip
+#define NSLineBreakByTruncatingHead     UILineBreakModeHeadTruncation
+#define NSLineBreakByTruncatingTail     UILineBreakByTruncatingTail
+#define NSLineBreakByTruncatingMiddle   UILineBreakModeMiddleTruncation
+
+#define NSTextAlignment                 UITextAlignment
+#define NSTextAlignmentLeft                UITextAlignmentLeft
+#define NSTextAlignmentCenter            UITextAlignmentCenter
+#define NSTextAlignmentRight            UITextAlignmentRight
+#endif
+
+#define isRetina ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 960), [[UIScreen mainScreen] currentMode].size) : NO)
+
+#define is4Inch  ([[UIScreen mainScreen] bounds].size.height == 568) ? TRUE : FALSE
+
