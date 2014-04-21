2347886dd03d8c906b49dcd4f59785df0cf81f4d
吴思聪 <sicongwu@anjuke.com>
2014-04-21 11:02:15 +0800

    add new controller

---
+++ AnjukeBroker_New/PublishFeatureViewController.h
@@ -0,0 +1,13 @@
+//
+//  PublishFeatureViewController.h
+//  AnjukeBroker_New
+//
+//  Created by paper on 14-4-21.
+//  Copyright (c) 2014年 Wu sicong. All rights reserved.
+//
+
+#import "RTViewController.h"
+
+@interface PublishFeatureViewController : RTViewController
+
+@end
