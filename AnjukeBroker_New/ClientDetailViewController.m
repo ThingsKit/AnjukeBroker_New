ccdcfec697ee4ca83a3efca489be288cbe38b741
吴思聪 <sicongwu@anjuke.com>
2014-04-24 14:52:53 +0800

    bar btn UI modifu

--- AnjukeBroker_New/ClientDetailViewController.m
+++ AnjukeBroker_New/ClientDetailViewController.m
@@ -54,9 +54,12 @@
     
     [self setTitleViewWithString:@"详细资料"];
     
-    UIBarButtonItem *moreItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"anjuke_icon_danye_more_.png"] style:UIBarButtonItemStylePlain target:self action:@selector(rightButtonAction:)];
+    UIBarButtonItem *moreItem = [UIBarButtonItem getBarButtonItemWithImage:[UIImage imageNamed:@"anjuke_icon_danye_more_.png"] highLihtedImg:[UIImage imageNamed:@"anjuke_icon_danye_more_.png"] taget:self action:@selector(rightButtonAction:)];
+    //[[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"anjuke_icon_danye_more_.png"] style:UIBarButtonItemStylePlain target:self action:@selector(rightButtonAction:)];
     
-    UIBarButtonItem *startItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"anjuke_icon_danye_noxingbiao_.png"] style:UIBarButtonItemStylePlain target:self action:@selector(redrawStarStatus:)];
+    UIBarButtonItem *startItem = [UIBarButtonItem getBarButtonItemWithImage:[UIImage imageNamed:@"anjuke_icon_danye_noxingbiao_.png"] highLihtedImg:[UIImage imageNamed:@"anjuke_icon_danye_noxingbiao_.png"] taget:self action:@selector(redrawStarStatus:)];
+    //[[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"anjuke_icon_danye_noxingbiao_.png"] style:UIBarButtonItemStylePlain target:self action:@selector(redrawStarStatus:)];
+    
     if (self.person.isStar) {
         startItem.image = [UIImage imageNamed:@"anjuke_icon_danye_xingbiao_.png"];
     }
