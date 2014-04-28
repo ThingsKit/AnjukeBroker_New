d1b31d6053f14d5fc21b9055d2d58e9d12917e67
your namejianzhongliu <your_email@youremail.com767381290@qq.com>
2014-04-28 17:58:55 +0800

    modify dictionary key-value

--- AnjukeBroker_New/ClientDetailViewController.m
+++ AnjukeBroker_New/ClientDetailViewController.m
@@ -175,12 +175,12 @@
     
     NSString *methodName = [NSString stringWithFormat:@"user/modifyFriendInfo/%@",[LoginManager getPhone]];
     
-    NSDictionary *params = @{@"to_uid":self.person.uid,
+    NSDictionary *params = @{@"to_uid":self.person.uid ? self.person.uid:@"",
                              @"is_star":isStar,
                              @"relation_cate_id":@"0",
-                             @"mark_name":self.person.markName,
-                             @"mark_phone":self.person.phone,
-                             @"mark_desc":self.person.markDesc};
+                             @"mark_name":self.person.markName ? self.person.markName:@"",
+                             @"mark_phone":self.person.phone ? self.person.phone:@"",
+                             @"mark_desc":self.person.markDesc ? self.person.markDesc:@""};
     [[RTRequestProxy sharedInstance] asyncRESTPostWithServiceID:RTAnjukeXRESTServiceID methodName:methodName params:params target:self action:@selector(onGetData:)];
 }
 
--- AnjukeBroker_New/ClientEditViewController.m
+++ AnjukeBroker_New/ClientEditViewController.m
@@ -256,12 +256,12 @@
     
     NSString *methodName = [NSString stringWithFormat:@"user/modifyFriendInfo/%@",[LoginManager getPhone]];
     
-    NSDictionary *params = @{@"to_uid":self.person.uid,
+    NSDictionary *params = @{@"to_uid":self.person.uid ? self.person.uid:@"",
                              @"is_star":isStar,
                              @"relation_cate_id":@"0",
-                             @"mark_name":self.person.markName,
-                             @"mark_phone":self.person.phone,
-                             @"mark_desc":self.person.markDesc};
+                             @"mark_name":self.person.markName ? self.person.markName:@"",
+                             @"mark_phone":self.person.phone ? self.person.phone:@"",
+                             @"mark_desc":self.person.markDesc ? self.person.markDesc:@""};
     //[NSMutableDictionary dictionaryWithObjectsAndKeys: self.person.uid ,@"to_uid" , isStar, @"is_star", @"0" ,@"relation_cate_id", self.person.markName ,@"mark_name", self.person.markPhone, @"mark_phone", self.person.markDesc, @"mark_desc",  nil];
     
     [[RTRequestProxy sharedInstance] asyncRESTPostWithServiceID:RTAnjukeXRESTServiceID methodName:methodName params:params target:self action:@selector(onGetData:)];
