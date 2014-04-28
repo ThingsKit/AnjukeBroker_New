56ec6f82e1ef849c5172e6083575ef6f8cbacf99
your namejianzhongliu <your_email@youremail.com767381290@qq.com>
2014-04-28 11:12:17 +0800

    解决6.x 下emoji后的中文变成粗体

--- AnjukeBroker_New/AX/Chat/libs/TTTAttributedLabel/AXTTTAttributedLabel.m
+++ AnjukeBroker_New/AX/Chat/libs/TTTAttributedLabel/AXTTTAttributedLabel.m
@@ -1222,6 +1222,60 @@ static inline NSRegularExpression * LinkRegularExpression() {
         mutableAttributedString = block(mutableAttributedString);
     }
     
+    if (SYSTEM_VERSION_LESS_THAN(@"7.0")) {
+        // 解决6.x 下emoji后的中文变成粗体
+        NSMutableAttributedString *fixAttributedString = [mutableAttributedString mutableCopy];
+        NSDictionary *attributes = @{NSFontAttributeName:[UIFont systemFontOfSize:15.99]};
+        
+        @try {
+            [[fixAttributedString string] enumerateSubstringsInRange:NSMakeRange(0, [text length]) options:NSStringEnumerationByComposedCharacterSequences usingBlock:^(NSString *substring, NSRange substringRange, NSRange enclosingRange, BOOL *stop) {
+                
+                BOOL returnValue = NO;
+                
+                const unichar hs = [substring characterAtIndex:0];
+                // surrogate pair
+                if (0xd800 <= hs && hs <= 0xdbff) {
+                    if (substring.length > 1) {
+                        const unichar ls = [substring characterAtIndex:1];
+                        const int uc = ((hs - 0xd800) * 0x400) + (ls - 0xdc00) + 0x10000;
+                        if (0x1d000 <= uc && uc <= 0x1f77f) {
+                            returnValue = YES;
+                        }
+                    }
+                } else if (substring.length > 1) {
+                    const unichar ls = [substring characterAtIndex:1];
+                    if (ls == 0x20e3) {
+                        returnValue = YES;
+                    }
+                    
+                } else {
+                    // non surrogate
+                    if (0x2100 <= hs && hs <= 0x27ff) {
+                        returnValue = YES;
+                    } else if (0x2B05 <= hs && hs <= 0x2b07) {
+                        returnValue = YES;
+                    } else if (0x2934 <= hs && hs <= 0x2935) {
+                        returnValue = YES;
+                    } else if (0x3297 <= hs && hs <= 0x3299) {
+                        returnValue = YES;
+                    } else if (hs == 0xa9 || hs == 0xae || hs == 0x303d || hs == 0x3030 || hs == 0x2b55 || hs == 0x2b1c || hs == 0x2b1b || hs == 0x2b50) {
+                        returnValue = YES;
+                    }
+                }
+                if (returnValue) {
+                    [fixAttributedString addAttributes:attributes range:substringRange];
+                }
+            }];
+            
+        }
+        @catch (NSException *exception) {
+            
+        }
+        [self setText:fixAttributedString];
+        return;
+    }
+    
+    
     [self setText:mutableAttributedString];
 }
 
