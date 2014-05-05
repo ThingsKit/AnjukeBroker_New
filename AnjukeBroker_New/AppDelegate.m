23b1ae7e6ceda26fa49bf13646ce0c200588f294
吴思聪 <sicongwu@anjuke.com>
2014-05-05 16:34:44 +0800

    update code

--- AnjukeBroker_New/AppDelegate.m
+++ AnjukeBroker_New/AppDelegate.m
@@ -115,7 +115,7 @@
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     dispatch_async(dispatch_get_current_queue(), ^{
-            [CrashLogUtil writeCrashLog];
+        [CrashLogUtil writeCrashLog];
     });
 }
 
