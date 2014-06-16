17f0ab64441d4a8142af42506698f9f59da21887
shanxu <kingundertree@163.com>
2014-06-16 15:39:50 +0800

    add no 竞价 view

---
+++ AnjukeBroker_New/AuctionForbidView.h
@@ -0,0 +1,15 @@
+//
+//  AuctionForbidView.h
+//  AnjukeBroker_New
+//
+//  Created by xiazer on 14-6-16.
+//  Copyright (c) 2014年 Wu sicong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AuctionForbidView : UIView
+
+- (instancetype)createForbidAuctionView;
+
+@end
